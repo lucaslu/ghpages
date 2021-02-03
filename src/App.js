4210c65fb0948d0d74cff3ee6ec901ce5a9a42a9
Lucas Lu <lucaslu@hotmail.com>
2021-02-02 20:19:13 -0800

    Little mod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lucas Lu 2021
         </p>
         <a
           className="App-link"
